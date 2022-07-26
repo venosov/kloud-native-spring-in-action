143b7af9134c1a3250c88be62ea92569f2c85ae5
vmartin <vmartin@gc.axesor.com>
2022-07-26 12:35:56 +0200

    m

---
+++ catalog-service/src/main/kotlin/com/polarbookshop/catalogservice/CatalogServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.polarbookshop.catalogservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogServiceApplication>(*args)
+}
