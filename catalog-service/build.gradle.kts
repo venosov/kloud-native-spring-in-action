ed51b9f0c287c4335515b408a96dabc89c121ece
vmartin <vmartin@gc.axesor.com>
2022-07-31 20:31:30 +0200

    m

--- catalog-service/build.gradle.kts
+++ catalog-service/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.withType<KotlinCompile> {
--- catalog-service/src/test/kotlin/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.kt
+++ catalog-service/src/test/kotlin/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.kt
@@ -1,13 +1,28 @@
 package com.polarbookshop.catalogservice
 
+import com.polarbookshop.catalogservice.domain.Book
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
-class CatalogServiceApplicationTests {
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+internal class CatalogServiceApplicationTests(@Autowired private val webTestClient: WebTestClient) {
 	@Test
-	fun contextLoads() {
+	fun whenPostRequestThenBookCreated() {
+		val expectedBook = Book("1231231231", "Title", "Author", 9.90)
+		webTestClient
+			.post()
+			.uri("/books")
+			.bodyValue(expectedBook)
+			.exchange()
+			.expectStatus().isCreated
+			.expectBody(Book::class.java).value { actualBook: Book ->
+				assertThat(actualBook)
+					.isNotNull
+				assertThat(actualBook.isbn)
+					.isEqualTo(expectedBook.isbn)
+			}
 	}
-
 }
